7c9b75d8793c96ce3ccabd03d7b67b5e05842cb5
giblecdg <giblecdg@gmail.com>
2025-12-06 23:29:07 +0100

    added menu to arithmetic function

--- scientific_calculator/main.cpp
+++ scientific_calculator/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <string>
 #include <limits>
+#include <chrono>
+#include <thread>
 
 void clearScreen(), doArithmetic(), doPower(), doRoot(), doTrigonometric(), settingsMenu(), printHelp();
 
@@ -12,12 +14,11 @@ int inputUserChoice(int min, int max);
 int main()
 {
     clearScreen();
-
+    std::cout << "=== SCIENTIFIC CALCULATOR ===\n";
+    
     while (true)
     {
-
-        std::cout << "=== SCIENTIFIC CALCULATOR ===\n";
-        std::cout << "1) Arithmetic operations (+, -, *, /)\n";
+        std::cout << "\n1) Arithmetic operations (+, -, *, /)\n";
         std::cout << "2) Power (a^b)\n";
         std::cout << "3) Root (square root, n-th root)\n";
         std::cout << "4) Trigonometric functions (sin, cos, tan)\n";
@@ -35,8 +36,12 @@ int main()
             // case 4: doTrigonometric(); break;
             // case 5: settingsMenu(); break;
             // case 6: printHelp(); break;
-            case 7: clearScreen(); std::cout << "Quitting...\n"; return 0;
-            default: std::cout << "\nWrong option. Please try again."; break;
+            case 7:
+                clearScreen(); 
+                std::cout << "Quitting...\n";
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                return 0;
+            default: break;
         }
     }
 }
@@ -50,11 +55,47 @@ void doArithmetic()
 
     while (true) 
     {
-        if (!inputDouble("Please enter a: ", "This is not a number", a)) { continue; }
-        if (!inputDouble("Please enter b: ", "This is not a number", b)) { continue; }
+        if (!inputDouble("Please enter a: ", "Error. This is not a number", a)) { continue; }
+        if (!inputDouble("Please enter b: ", "Error. This is not a number", b)) { continue; }
 
-        std::cout << a << " + " << b << " = " << a+b << "\n\n";
-        break; 
+        char userChoice;
+        std::cout << "\n+ : ADD";
+        std::cout << "\n- : SUBSTRACT";
+        std::cout << "\n* : MULTIPLY";
+        std::cout << "\n/ : DIVIDE\n\n";
+
+        std::cout << "Enter your choice: ";
+        std::cin >> userChoice;
+
+        switch (userChoice) 
+        {
+            case '+':
+                std::cout << a << " + " << b << " = " << a+b << "\n";
+                break;
+            case '-':
+                std::cout << a << " - " << b << " = " << a-b << "\n";
+                break;
+            case '*':
+                std::cout << a << " * " << b << " = " << a*b << "\n";
+                break;
+            case '/':
+                if (b == 0)
+                {
+                    std::cout << "Error. You can't divide by 0\n";
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                    break;
+                }
+
+                std::cout << a << " / " << b << " = " << a/b << "\n";
+                break;
+            default:
+                std::cout << "Wrong option. Please try again.\n";
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+                break;
+        }
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        break;
     }
 }
 
@@ -99,11 +140,11 @@ int inputUserChoice(int min, int max)
                 return userChoice;
             }
 
-            std::cout << "\nWrong option. Please try again.\n";
+            std::cout << "Wrong option. Please try again.\n";
         }
         else
         {
-            std::cout << "\nWrong option. Please try again.\n";
+            std::cout << "Wrong option. Please try again.\n";
         }
 
         std::cin.clear();
